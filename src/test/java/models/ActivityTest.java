90f8f55acb17b62ec1f9643db4ac42def45d55cf
Shane Nolan <shane.nolan.wit@gmail.com>
2016-10-18 22:06:37 +0100

    test commit from eclipse

--- src/test/java/models/ActivityTest.java
+++ src/test/java/models/ActivityTest.java
@@ -11,32 +11,28 @@ import utils.DateTimeUtils;
 
 import static models.Fixtures.activities;
 
-public class ActivityTest
-{
-	Activity test = new Activity ("walk",  "fridge", 0.001, DateTimeUtils.convertStringToLocalDateTime("12:10:2013 9:00:00"));
+public class ActivityTest {
+	Activity test = new Activity("walk", "fridge", 0.001,
+			DateTimeUtils.convertStringToLocalDateTime("12:10:2013 9:00:00"));
 
 	@Test
-	public void testCreate()
-	{
-		assertEquals ("walk",          test.type);
-		assertEquals ("fridge",        test.location);
-		assertEquals (0.0001, 0.001,   test.distance);    
+	public void testCreate() {
+		assertEquals("walk", test.type);
+		assertEquals("fridge", test.location);
+		assertEquals(0.0001, 0.001, test.distance);
 	}
 
 	@Test
-	public void testIds()
-	{
+	public void testIds() {
 		Set<Long> ids = new HashSet<>();
-		for (Activity activity : activities)
-		{
+		for (Activity activity : activities) {
 			ids.add(activity.id);
 		}
-		assertEquals (activities.length, ids.size());
+		assertEquals(activities.length, ids.size());
 	}
 
 	@Test
-	public void testToString()
-	{
-		assertEquals ("Activity{" + test.id + ", walk, fridge, 0.001, 2013-10-12T09:00, []}", test.toString());
+	public void testToString() {
+		assertEquals("Activity{" + test.id + ", walk, fridge, 0.001, 2013-10-12T09:00, []}", test.toString());
 	}
 }
